c35f9667c936f4d30c311df8e7aed8d430aad462
Vedant Dange <vedantdange149@gmail.com>
2025-07-11 22:50:04 +0530

    First Commit

---
+++ client/src/pages/Home..jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+      <FeaturedDestination/>
+      <ExclusiveOffers/>
+    </>
+  )
+}
+
+export default Home
